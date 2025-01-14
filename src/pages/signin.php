ac41ab114db448d55b9d9eb04813d7c00d1a0cc9
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-14 14:49:07 +0300

    style Folder Added

---
+++ src/pages/signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link type="text/css" rel="stylesheet" href="../styles/globals.css">
+  <link rel="stylesheet" href="../styles/signin.css">
+  <title>Signin</title>
+</head>
+<body>
+  <section>
+    <h2>Mawsemy</h2>
+  </section>
+  <main>
+    <h1>New Signup</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Google</a></li>
+        <li><a href="#">X</a></li>
+      </ul>
+    </nav>
+  </main>
+</body>
+</html>
