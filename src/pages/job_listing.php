196a011f363ba1c1d3a64ac57346928c5041217c
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-17 11:20:22 +0300

    header completed

--- src/pages/job_listing.php
+++ src/pages/job_listing.php
@@ -10,7 +10,22 @@
   <section class="page-structure">
       <?php include "../components/NavBar.php" ?>
     <main>
-
+      <header>
+        <div class="username">
+          <a href="./profile.php">
+            <img src="../images/profile.svg" alt="شعار المستخدم">
+            <h2>اسم المستخدم</h2>
+          </a>
+        </div>
+        <div class="options">
+          <a href="#">
+            <img class="nav-icon" src="../images/settings.svg" alt="شعار الإعدادت">
+          </a>
+          <a href="#">
+            <img class="nav-icon" style="--color: #DF4F4F" src="../images/logout.svg" alt="شعار تسجيل الدخول">
+          </a>
+        </div>
+      </header>
     </main>
   </section>
 </body>
