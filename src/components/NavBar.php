385f627ccc440b3e253cc790c03bfaf711159d46
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-17 10:48:36 +0300

    navbar completed + add new components folder + more config in globals.css

---
+++ src/components/NavBar.php
@@ -0,0 +1,20 @@
+<nav class="main-navigation">
+      <a href="./index.php" class="logo-link">
+        <img src="Logo.png" alt="موسمي">
+      </a>
+      
+      <ul>
+        <li>
+          <a href="./profile.php" class="nav-path">
+            سيرتي
+            <img src="../images/cv.svg" alt="شعار للسيرة الذاتية" >
+          </a>
+        </li>
+        <li>
+          <a href="./company.php" class="nav-path">
+            الشركات
+            <img src="../images/company.svg" alt="شعار لشركة" >
+          </a>
+        </li>
+      </ul>
+    </nav>
---
+++ src/pages/job_listing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/job_listing.css">
+  <title>Document</title>
+</head>
+<body>
+  <section class="page-structure">
+      <?php include "../components/NavBar.php" ?>
+    <main>
+
+    </main>
+  </section>
+</body>
+</html>
