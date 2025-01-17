d0e7584e588008b9dc0f0d1e89b507d915cea427
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-01-17 16:58:11 +0300

    jobs section added

--- src/pages/job_listing.php
+++ src/pages/job_listing.php
@@ -28,7 +28,7 @@
       </header>
 
       <main>
-        <h1 class="heading-title">أحدث الوظائف</h1>
+        <h1 class="heading-title">أحدث الوظائف : </h1>
 
         <div class="content">
           <div class="filter-section">
@@ -46,7 +46,83 @@
               <input type="text" name="company-name">
             </form>
           </div>
-          <div class="jobs-section"></div>
+          <div class="jobs-section">
+            <div class="job">
+              <img src="../images/sampleJob.png" alt="job image">
+              <div class="content">
+                <div class="titles">
+                  <h2 class="job-title">اسم الوظيفة</h2>
+                  <h3 class="company-title">اسم الشركة</h3>
+                  <div class="keywords">
+                    <span class="keyword">الكلمة المفتاحية 1</span>
+                    <span class="keyword">الكلمة المفتاحية 2</span>
+                  </div>
+                </div>
+                <p class="description">وظيفة موسمية براتب مجزي وبدلات و بيئة متطورة و الخ... </p>
+              </div>
+              <a href="#"></a>
+            </div>
+            <div class="job">
+              <img src="../images/sampleJob.png" alt="job image">
+              <div class="content">
+                <div class="titles">
+                  <h2 class="job-title">اسم الوظيفة</h2>
+                  <h3 class="company-title">اسم الشركة</h3>
+                  <div class="keywords">
+                    <span class="keyword">الكلمة المفتاحية 1</span>
+                    <span class="keyword">الكلمة المفتاحية 2</span>
+                  </div>
+                </div>
+                <p class="description">وظيفة موسمية براتب مجزي وبدلات و بيئة متطورة و الخ... </p>
+              </div>
+              <a href="#"></a>
+            </div>
+            <div class="job">
+              <img src="../images/sampleJob.png" alt="job image">
+              <div class="content">
+                <div class="titles">
+                  <h2 class="job-title">اسم الوظيفة</h2>
+                  <h3 class="company-title">اسم الشركة</h3>
+                  <div class="keywords">
+                    <span class="keyword">الكلمة المفتاحية 1</span>
+                    <span class="keyword">الكلمة المفتاحية 2</span>
+                  </div>
+                </div>
+                <p class="description">وظيفة موسمية براتب مجزي وبدلات و بيئة متطورة و الخ... </p>
+              </div>
+              <a href="#"></a>
+            </div>
+            <div class="job">
+              <img src="../images/sampleJob.png" alt="job image">
+              <div class="content">
+                <div class="titles">
+                  <h2 class="job-title">اسم الوظيفة</h2>
+                  <h3 class="company-title">اسم الشركة</h3>
+                  <div class="keywords">
+                    <span class="keyword">الكلمة المفتاحية 1</span>
+                    <span class="keyword">الكلمة المفتاحية 2</span>
+                  </div>
+                </div>
+                <p class="description">وظيفة موسمية براتب مجزي وبدلات و بيئة متطورة و الخ... </p>
+              </div>
+              <a href="#"></a>
+            </div>
+            <div class="job">
+              <img src="../images/sampleJob.png" alt="job image">
+              <div class="content">
+                <div class="titles">
+                  <h2 class="job-title">اسم الوظيفة</h2>
+                  <h3 class="company-title">اسم الشركة</h3>
+                  <div class="keywords">
+                    <span class="keyword">الكلمة المفتاحية 1</span>
+                    <span class="keyword">الكلمة المفتاحية 2</span>
+                  </div>
+                </div>
+                <p class="description">وظيفة موسمية براتب مجزي وبدلات و بيئة متطورة و الخ... </p>
+              </div>
+              <a href="#"></a>
+            </div>
+          </div>
         </div>
       </main>
     </section>
